d44c280e4a20316d4fec0746b14614f4a0897f08
dmartinez <david316_valdes@hotmail.com>
2019-10-14 22:56:13 -0500

    Corrected endpoint url

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pokemon Battle Simulator', server_name:'https://sleepy-plains-58854.herokuapp.com/' });
+  res.render('index', { title: 'Pokemon Battle Simulator', server_name:'https://lit-reaches-98947.herokuapp.com/' });
 });
 
 module.exports = router;
